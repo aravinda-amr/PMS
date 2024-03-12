d5401e63d3fd97bb6018c9d7f137a1fa6605a293
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-12 16:54:59 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express()
 
-app.listen(4000,() => {
+app.listen(process.env.PORT,() => {
     console.log('listening on port 4000')
 })
+
