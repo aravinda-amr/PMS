042563d762b504e4b43cf6cd55dd5516b1e06abc
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-13 01:05:05 +0530

    created login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,16 @@ import User from '../models/userModel.js';
 
 // login user
 export const loginUser = async (req, res) => {
-    res.json({ message: 'Login route' });
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email, password);
+
+        res.status(200).json({ email, user });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+
 }
 
 // signup user
