e4658163b69098ced26210573f352b4d99fd4eae
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-16 18:54:28 +0530

    Update requireAuth.js

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
-const requireAuth = async (req, res, next) => {
+const requireAuth = async(req, res, next) => {
 
     //verify authentication
     const { authorization } = req.headers
 
-    if(!authorization){
+    if(!authorization) {
         return res.status(401).json({ error: 'Authorization token required' })
     }
     
@@ -19,8 +19,7 @@ const requireAuth = async (req, res, next) => {
 
         req.user = await User.findOne({ _id }).select('_id')
         next()  //fire the next handler function
-
-        req.user = 
+        
     } catch (error) {
         console.log(error)
         res.status(401).json({ error: 'Request is not authorized' })
