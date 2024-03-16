05cbeac615d5a18816fbb7a44c75d83ffd3050e3
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-16 13:03:50 +0530

    created logout button and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //Pages & Components
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route
