c2c56a46917d76a4b2c983fa3d85ac98448eed62
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-16 12:11:45 +0530

    added name and contact to signup

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -6,14 +6,14 @@ export const useSignup = () => {
     const [isLoading, setIsLoading] = useState(null);
     const { dispatch } = useAuthContext();
 
-    const signup = async (email, password) => {
+    const signup = async (email, password, name, contact ) => {
         setIsLoading(true)
         setError(null)
         
         const response = await fetch('/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ email, password })
+            body: JSON.stringify({ email, password, name, contact })
         })
 
         const json = await response.json()
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,17 +4,33 @@ import { useSignup } from '../hooks/useSignup';
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [contact, setContact] = useState('');
     const { signup, isLoading, error } = useSignup();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        await signup(email, password);
+        await signup(email, password, name, contact);
     }
 
     return (
         <form className="signup" onSubmit={handleSubmit}>
             <h3>Signup</h3>
+
+            <label>Name</label>
+            <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+            />
+
+            <label>Contact</label>
+            <input
+                type="text"
+                onChange={(e) => setContact(e.target.value)}
+                value={contact}
+            />
 
             <label>Email</label>
             <input
