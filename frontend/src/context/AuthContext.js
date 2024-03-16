aa0f5b995f028f900668ff97b52b3a8f5e699e61
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-16 15:57:20 +0530

    update AuthContext.js
    implement useEffect to check whether a user is already in localstorage

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -15,6 +15,13 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, { user: null });
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user) {
+            dispatch({ type: 'LOGIN', payload: user });
+        }
+    }, []);
 
     //can see the state in console when login or logout
     console.log('AuthConetxt State: ', state);
