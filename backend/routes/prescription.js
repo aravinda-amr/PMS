78369208061d49c58486f71d213e990a507cde94
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-17 19:24:50 +0530

    created prescription routes and updated the server.js
    created backend for the prescriptions without upload and updated the server.js

---
+++ backend/routes/prescription.js
@@ -0,0 +1,22 @@
+import expres from 'express';
+import { 
+    getPrescriptions,
+    getPrescription,
+    createPrescription, 
+    deletePrescription } from '../controllers/prescriptionController';
+
+const router = express.Router();
+
+//get all prescriptions
+router.get('/', getPrescriptions)
+
+//get a single prescription
+router.get('/:id', getPrescription)
+
+//create a prescription
+router.post('/', createPrescription)
+
+//delete a prescription
+router.delete('/:id', deletePrescription)
+
+export default router;
