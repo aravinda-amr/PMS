05b1a8be4a9d61e327238aa0790d4a0cbc08a7cc
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-17 17:59:51 +0530

    update app.js
    created home page and redirect the user according to the auth status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 //Pages & Components
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 
 
 function App() {
+  const { user } = useAuthContext();
+
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
+            <Route 
+              path="/"
+              element={user ? <Home/> : <Navigate to="/login" />}
+            />
             <Route
               path="/login"
-              element={<Login />} 
+              element={!user ? <Login /> : <Navigate to="/" />} 
             />
             <Route 
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
