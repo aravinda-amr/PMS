aba01d9d8e2fb257eec2f3d374c33c092b3d25f6
Tishie <tishigunawardhana01@gmail.com>
2024-03-19 13:30:17 +0530

    Create billingModel.js

---
+++ backend/models/billingModel.js
@@ -0,0 +1,28 @@
+
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const billingSchema = new Schema({
+    
+    customerID: {
+        type: String,
+        required: true
+    },
+    invoiceDate: {
+        type: Date,
+        required: true
+    },
+    medicines:[{
+        drugName: String,
+        purchaseQuantity: Number,
+        unitPrice: Number,
+        calculateItemTotal: Number,
+
+    }]
+   
+   
+}, {timestamps: true});
+
+
+export default mongoose.model('Bill', billingSchema)
