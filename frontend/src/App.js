14a3f604906136dff38a56a9dd418b5ab1b55b1a
Tishie <tishigunawardhana01@gmail.com>
2024-03-22 02:37:23 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import AboutExpire from './pages/AboutExpire';
 import OutOfStock from './pages/OutOfStock';
 import Prescription from './pages/Prescription';
 import Leave from './pages/Leave'
+import Billing from './pages/Billing';
+import NewSale from './pages/NewSale';
 
 
 function App() {
@@ -69,6 +71,14 @@ function App() {
             <Route
               path="/prescription"
               element={<Prescription/>}
+            />
+            <Route
+              path="/billing"
+              element={<Billing/>}
+            />
+            <Route
+              path="/new-sale"
+              element={<NewSale/>}
             />
           </Routes>
         </div>
