e3b2282a98cbe6491c931e8ba93822997c3c3984
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-03-21 21:20:35 +0530

    edited useLogout and prescription.js

--- frontend/src/pages/Prescription.js
+++ frontend/src/pages/Prescription.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { usePrescriptionContext } from "../hooks/usePrescription";
+import { useAuthContext } from '../hooks/useAuthContext';
 
 //importing the components
 import PrescritpionUpload from "../components/PrescriptionUpload";
@@ -7,11 +8,19 @@ import PrescriptionDetails from "../components/PrescriptionDetails";
 
 const Prescription = () => {
     const { prescriptions, dispatch } = usePrescriptionContext();
+    const { user } = useAuthContext();
     
     useEffect(() => {
         const fetchPrescriptions = async () => {
-        const response = await fetch("/api/prescription");
+        const response = await fetch("/api/prescription", {
+            method: "GET",
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.token}`
+            }
+        });
         const json = await response.json();
+        console.log(json);
         if (response.ok) {
             dispatch({ type: "SET_PRESCRIPTIONS", payload: json });
         }
@@ -20,7 +29,7 @@ const Prescription = () => {
     }, [dispatch]);
     
     return (
-        <div className="prescription">
+        <div className="ml-64">
         <h2>Prescriptions</h2>
         <div className="prescription_list">
             {prescriptions &&
