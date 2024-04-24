7f499e6b6b0d363da6ac128d33a63e092d2037d2
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-04-24 16:46:10 +0530

    done quotation form

---
+++ frontend/src/components/QuotationForm.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
+import POSSearch from './PosSearch';
+
+
+const QuotationForm = () => {
+    const [prescriptionID, setPrescriptionID] = useState('');
+    const [invoiceDate, setInvoiceDate] = useState('');
+    const [medicines, setMedicines] = useState([]);
+    const [subTotal, setSubTotal] = useState(0);
+
+    const handleAddToBill = (drug) => {
+        const existingMedicineIndex = medicines.findIndex(item => item.drugName === drug.drugName);
+
+        if (existingMedicineIndex !== -1) {
+            const updatedMedicines = [...medicines];
+            updatedMedicines[existingMedicineIndex].purchaseQuantity += 1;
+            setMedicines(updatedMedicines);
+        } else {
+            const medicine = { drugName: drug.drugName, price: drug.price, purchaseQuantity: 1 };
+            setMedicines([...medicines, medicine]);
+        }
+    };
+
+    const handleGenerateQuotation = () => {
+        // Calculate subtotal
+        const total = medicines.reduce((acc, curr) => acc + (curr.price * curr.purchaseQuantity), 0);
+        setSubTotal(total);
+    };
+
+    const handleDeleteMedicine = (index) => {
+        const updatedMedicines = [...medicines];
+        updatedMedicines.splice(index, 1);
+        setMedicines(updatedMedicines);
+    };
+
+    const handlePurchaseQuantityChange = (e, index) => {
+        const updatedMedicines = [...medicines];
+        const newQuantity = parseInt(e.target.value);
+
+        if (!isNaN(newQuantity) && newQuantity >= 1) {
+            updatedMedicines[index].purchaseQuantity = newQuantity;
+            setMedicines(updatedMedicines);
+        }
+    };
+
+    return (
+        <div className="max-w-custom mx-auto p-6 bg-white shadow-md rounded-lg">
+            <h3 className="text-2xl font-semibold mb-4 text-gray-800">Create Quotation</h3>
+            <POSSearch handleAddToBill={handleAddToBill} />
+
+            <form onSubmit={handleGenerateQuotation}>
+                <div className="grid grid-cols-3 gap-4 mb-4">
+                    <div>
+                        <label htmlFor="prescriptionID" className="block text-sm font-semibold text-gray-800">Prescription ID:</label>
+                        <input
+                            type="text"
+                            id="prescriptionID"
+                            value={prescriptionID}
+                            onChange={(e) => setPrescriptionID(e.target.value)}
+                            required
+                            className="input-field"
+                        />
+                    </div>
+                    
+                </div>
+                <div>
+                    <h4 className="text-lg font-semibold mb-2 text-gray-800">Medicines</h4>
+                    <table className="w-full mb-4">
+                        <thead>
+                            <tr>
+                                <th className="px-4 py-2 text-gray-800">Index</th>
+                                <th className="px-4 py-2 text-gray-800">Drug Name</th>
+                                <th className="px-4 py-2 text-gray-800">Unit Price</th>
+                                <th className="px-4 py-2 text-gray-800">Purchase Quantity</th>
+                                <th className="px-4 py-2 text-gray-800">Item Total</th>
+                                <th className="px-4 py-2 text-gray-800">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {medicines.map((medicine, index) => (
+                                <tr key={index}>
+                                    <td className="border px-4 py-2">{index + 1}</td>
+                                    <td className="border px-4 py-2">{medicine.drugName}</td>
+                                    <td className="border px-4 py-2">{medicine.price}</td>
+                                    <td className="border px-4 py-2">
+                                        <input
+                                            type="number"
+                                            value={medicine.purchaseQuantity}
+                                            onChange={(e) => handlePurchaseQuantityChange(e, index)}
+                                            required
+                                            className="input-field"
+                                        />
+                                    </td>
+                                    <td className="border px-4 py-2">{(medicine.price * medicine.purchaseQuantity).toFixed(2)}</td>
+                                    <td className="border px-4 py-2">
+                                        <button onClick={() => handleDeleteMedicine(index)} className="text-red-500">
+                                            <FaTrash />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <button type="submit" className="bg-login1 hover:bg-login2 text-black font-bold px-4 py-1 rounded-lg font-jakarta cursor-pointer hover:transition-all">Generate Quotation</button>
+            </form>
+            <div>
+                <h4 className="text-lg font-semibold mb-2 text-gray-800">Subtotal: LKR{subTotal.toFixed(2)}</h4>
+            </div>
+            {subTotal > 0 && (
+                <button className="bg-login1 hover:bg-login2 text-white font-bold px-4 py-1 rounded-lg font-jakarta cursor-pointer hover:transition-all">Send Quotation</button>
+            )}
+        </div>
+    );
+};
+
+export default QuotationForm;
