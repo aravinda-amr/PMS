1320283431bd97bdd78619b3728460b526a72747
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-04-25 17:43:38 +0530

    Update QuotationForm.js
    edited frontend. subtotal will update as soon as drugs add

--- frontend/src/components/QuotationForm.js
+++ frontend/src/components/QuotationForm.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import POSSearch from './PosSearch';
 
-
 const QuotationForm = () => {
     const [prescriptionID, setPrescriptionID] = useState('');
-    const [invoiceDate, setInvoiceDate] = useState('');
     const [medicines, setMedicines] = useState([]);
     const [subTotal, setSubTotal] = useState(0);
+
+    useEffect(() => {
+        // Calculate subtotal whenever medicines or their quantities change
+        const total = medicines.reduce((acc, curr) => acc + (curr.price * curr.purchaseQuantity), 0);
+        setSubTotal(total);
+    }, [medicines]);
 
     const handleAddToBill = (drug) => {
         const existingMedicineIndex = medicines.findIndex(item => item.drugName === drug.drugName);
@@ -20,12 +24,6 @@ const QuotationForm = () => {
             const medicine = { drugName: drug.drugName, price: drug.price, purchaseQuantity: 1 };
             setMedicines([...medicines, medicine]);
         }
-    };
-
-    const handleGenerateQuotation = () => {
-        // Calculate subtotal
-        const total = medicines.reduce((acc, curr) => acc + (curr.price * curr.purchaseQuantity), 0);
-        setSubTotal(total);
     };
 
     const handleDeleteMedicine = (index) => {
@@ -49,62 +47,45 @@ const QuotationForm = () => {
             <h3 className="text-2xl font-semibold mb-4 text-gray-800">Create Quotation</h3>
             <POSSearch handleAddToBill={handleAddToBill} />
 
-            <form onSubmit={handleGenerateQuotation}>
-                <div className="grid grid-cols-3 gap-4 mb-4">
-                    <div>
-                        <label htmlFor="prescriptionID" className="block text-sm font-semibold text-gray-800">Prescription ID:</label>
-                        <input
-                            type="text"
-                            id="prescriptionID"
-                            value={prescriptionID}
-                            onChange={(e) => setPrescriptionID(e.target.value)}
-                            required
-                            className="input-field"
-                        />
-                    </div>
-                    
-                </div>
-                <div>
-                    <h4 className="text-lg font-semibold mb-2 text-gray-800">Medicines</h4>
-                    <table className="w-full mb-4">
-                        <thead>
-                            <tr>
-                                <th className="px-4 py-2 text-gray-800">Index</th>
-                                <th className="px-4 py-2 text-gray-800">Drug Name</th>
-                                <th className="px-4 py-2 text-gray-800">Unit Price</th>
-                                <th className="px-4 py-2 text-gray-800">Purchase Quantity</th>
-                                <th className="px-4 py-2 text-gray-800">Item Total</th>
-                                <th className="px-4 py-2 text-gray-800">Actions</th>
+            <div>
+                <h4 className="text-lg font-semibold mb-2 text-gray-800">Medicines</h4>
+                <table className="w-full mb-4">
+                    <thead>
+                        <tr>
+                            <th className="px-4 py-2 text-gray-800">Index</th>
+                            <th className="px-4 py-2 text-gray-800">Drug Name</th>
+                            <th className="px-4 py-2 text-gray-800">Unit Price</th>
+                            <th className="px-4 py-2 text-gray-800">Purchase Quantity</th>
+                            <th className="px-4 py-2 text-gray-800">Item Total</th>
+                            <th className="px-4 py-2 text-gray-800">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {medicines.map((medicine, index) => (
+                            <tr key={index}>
+                                <td className="border px-4 py-2">{index + 1}</td>
+                                <td className="border px-4 py-2">{medicine.drugName}</td>
+                                <td className="border px-4 py-2">{medicine.price}</td>
+                                <td className="border px-4 py-2">
+                                    <input
+                                        type="number"
+                                        value={medicine.purchaseQuantity}
+                                        onChange={(e) => handlePurchaseQuantityChange(e, index)}
+                                        required
+                                        className="input-field"
+                                    />
+                                </td>
+                                <td className="border px-4 py-2">{(medicine.price * medicine.purchaseQuantity).toFixed(2)}</td>
+                                <td className="border px-4 py-2">
+                                    <button onClick={() => handleDeleteMedicine(index)} className="text-red-500">
+                                        <FaTrash />
+                                    </button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {medicines.map((medicine, index) => (
-                                <tr key={index}>
-                                    <td className="border px-4 py-2">{index + 1}</td>
-                                    <td className="border px-4 py-2">{medicine.drugName}</td>
-                                    <td className="border px-4 py-2">{medicine.price}</td>
-                                    <td className="border px-4 py-2">
-                                        <input
-                                            type="number"
-                                            value={medicine.purchaseQuantity}
-                                            onChange={(e) => handlePurchaseQuantityChange(e, index)}
-                                            required
-                                            className="input-field"
-                                        />
-                                    </td>
-                                    <td className="border px-4 py-2">{(medicine.price * medicine.purchaseQuantity).toFixed(2)}</td>
-                                    <td className="border px-4 py-2">
-                                        <button onClick={() => handleDeleteMedicine(index)} className="text-red-500">
-                                            <FaTrash />
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-                <button type="submit" className="bg-login1 hover:bg-login2 text-black font-bold px-4 py-1 rounded-lg font-jakarta cursor-pointer hover:transition-all">Generate Quotation</button>
-            </form>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <div>
                 <h4 className="text-lg font-semibold mb-2 text-gray-800">Subtotal: LKR{subTotal.toFixed(2)}</h4>
             </div>
