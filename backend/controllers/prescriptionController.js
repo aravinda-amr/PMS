ed9669106d091b2a9d6c1f2b878c34779c2ad0ab
Aravinda Amaratunge <aamaratunge202@gmail.com>
2024-04-26 21:32:09 +0530

    Update prescriptionController.js
    commented out

--- backend/controllers/prescriptionController.js
+++ backend/controllers/prescriptionController.js
@@ -1,62 +1,70 @@
-import Prescriptions from '../models/prescriptionModel.js';
-import mongoose from 'mongoose';
+// import Prescriptions from '../models/prescriptionModel.js';
+// import mongoose from 'mongoose';
 
-//get all prescriptions
-const getPrescriptions = async (req, res) => {
-    const user_id = req.user._id;
-    const prescriptions = await Prescriptions.find({ user_id }).sort({createdAt: -1});
 
-    res.status(200).json(prescriptions);
-}
 
-//get a single prescription
-const getPrescription = async (req, res) => {
-    const {id} = req.params;
+// //-----------------------------------------------------
+// //-------------this file is not in use-----------------
+// //-----------------------------------------------------
+
+
+
+// //get all prescriptions
+// const getPrescriptions = async (req, res) => {
+//     const user_id = req.user._id;
+//     const prescriptions = await Prescriptions.find({ user_id }).sort({createdAt: -1});
+
+//     res.status(200).json(prescriptions);
+// }
+
+// //get a single prescription
+// const getPrescription = async (req, res) => {
+//     const {id} = req.params;
     
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such prescription'});
-    }
+//     if(!mongoose.Types.ObjectId.isValid(id)) {
+//         return res.status(404).json({error: 'No such prescription'});
+//     }
     
-    const prescription = await Prescriptions.findById(id);
+//     const prescription = await Prescriptions.findById(id);
 
-    if(!prescription) {
-        return res.status(404).json({error: 'No such prescription'});
-    }
+//     if(!prescription) {
+//         return res.status(404).json({error: 'No such prescription'});
+//     }
 
-    res.status(200).json(prescription);
-}
+//     res.status(200).json(prescription);
+// }
 
-//create a prescription
-const createPrescription = async (req, res) => {
-    const note  = req.body.note;
-    const substitutes  = req.body.substitutes;
-    const prescriptionImg  = req.body.image;
+// //create a prescription
+// const createPrescription = async (req, res) => {
+//     const note  = req.body.note;
+//     const substitutes  = req.body.substitutes;
+//     const prescriptionImg  = req.body.image;
 
-//add document to DB
-    try {
-        const userId = req.user._id;
-        const prescription = await Prescriptions.create({note, substitutes, userId, prescriptionImg});
-        res.status(200).json(prescription);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-}
+// //add document to DB
+//     try {
+//         const userId = req.user._id;
+//         const prescription = await Prescriptions.create({note, substitutes, userId, prescriptionImg});
+//         res.status(200).json(prescription);
+//     } catch (error) {
+//         res.status(400).json({error: error.message});
+//     }
+// }
 
-//delete a prescription
-const deletePrescription = async (req, res) => {
-    const {id} = req.params;
+// //delete a prescription
+// const deletePrescription = async (req, res) => {
+//     const {id} = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such prescription'});
-    }
+//     if(!mongoose.Types.ObjectId.isValid(id)) {
+//         return res.status(404).json({error: 'No such prescription'});
+//     }
 
-    const prescription = await Prescriptions.findOneAndDelete({_id: id});
+//     const prescription = await Prescriptions.findOneAndDelete({_id: id});
 
-    if(!prescription) {
-        return res.status(404).json({error: 'No such prescription'});
-    }
+//     if(!prescription) {
+//         return res.status(404).json({error: 'No such prescription'});
+//     }
 
-    res.status(200).json(prescription);
-}
+//     res.status(200).json(prescription);
+// }
 
-export {getPrescriptions, getPrescription, createPrescription, deletePrescription}
+// export {getPrescriptions, getPrescription, createPrescription, deletePrescription}
