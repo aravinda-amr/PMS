edc187547ac08cb1955b3b001dc88932957e4c91
gladiya <127586661+GladiyaM@users.noreply.github.com>
2024-04-27 14:29:38 +0530

    Update Leave.js

--- frontend/src/pages/Leave.js
+++ frontend/src/pages/Leave.js
@@ -1,12 +1,21 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useLeavesContext} from '../hooks/useLeavesContext'
-
+import Button from '@mui/material/Button';
 // components
 import LeaveDetails from '../components/LeaveDetails'
 import LeavesForm from '../components/LeaveForm'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import EmailModal from './email.js'; 
 
 const Leave = () => {
   const {leaves, dispatch} = useLeavesContext()
+
+  
+  const [emailModalOpen, setEmailModalOpen] = useState(false);
+
+  const handleEmailModalClose = () => {
+     setEmailModalOpen(false);
+  }
 
   useEffect(() => {
 
@@ -23,8 +32,53 @@ const Leave = () => {
     fetchLeaves()
   },[dispatch])
 
+  const handleEmailModalSubmit = async (emailDetails) => {
+    try {
+      const response = await fetch('/api/email/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(emailDetails),
+      });
+
+      if (response.ok) {
+        alert('Email sent successfully');
+      } else {
+        alert('Failed to send email');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('Failed to send email');
+    }
+ };
+
   return(
     <div className="ml-64">
+
+<Button
+        variant="outlined"
+        size="small"
+        onClick={() => setEmailModalOpen(true)}
+        className="text-white px-4 py-1 rounded-lg font-semibold cursor-pointer hover:transition-all"
+        sx={{
+          backgroundColor: '#00BFFF', // Sets the background color to light blue
+          color: 'white', // Sets the text color to white
+          '&:hover': {
+            backgroundColor: 'blue', // Adjust hover color as needed
+          },
+          marginRight: '10px', // Adds space from the right side of the button
+        }}
+      >
+       
+        Send Email
+       <MailOutlineIcon sx={{ marginLeft: '5px' }} /> {/* Adds an icon to the button */}
+      </Button>
+      <EmailModal
+        open={emailModalOpen}
+        handleClose={handleEmailModalClose}
+        handleSubmit={handleEmailModalSubmit}
+      />
   
       <LeavesForm />
         {leaves && leaves.map((leave) =>(
