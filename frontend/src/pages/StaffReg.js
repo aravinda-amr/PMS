9ca2637334206fe0d8efd668afb2749d4dcfecd7
Chathura Theekshana <chathuradayananda1@gmail.com>
2024-05-06 07:35:16 +0530

    user creation styles added

--- frontend/src/pages/StaffReg.js
+++ frontend/src/pages/StaffReg.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Button, TextField, Typography, Container, Box, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
+import { useSignup } from '../hooks/useSignup';
 
 const StaffReg = () => {
     const [email, setEmail] = useState('');
@@ -7,17 +9,15 @@ const StaffReg = () => {
     const [contact, setContact] = useState('');
     const [selectedRole, setSelectedRole] = useState('');
     const [errors, setErrors] = useState({});
-    //const { signup, isLoading, error } = useSignup();
+    const { signup, isLoading, error } = useSignup();
 
-
-    //validate password
+    // Validate password
     const isStrongPassword = (password) => {
-        const strongPasswordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const strongPasswordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$%*?&])[A-Za-z\d@$%*?&]{8,}$/;
         return strongPasswordRegex.test(password);
     }
 
-
-    //validate contact
+    // Validate contact
     const isValidContact = (contact) => {
         const contactRegex = /^\d{10}$/;
         return contactRegex.test(contact);
@@ -40,7 +40,7 @@ const StaffReg = () => {
             setErrors(prevErrors => ({ ...prevErrors, contact: 'Contact must be exactly 10 numbers' }));
             return;
         }
-        
+
         if (!email.trim()) {
             setErrors(prevErrors => ({ ...prevErrors, email: 'Email is required' }));
             return;
@@ -51,143 +51,111 @@ const StaffReg = () => {
             return;
         }
 
-        if (!isStrongPassword(password)) {
-            setErrors(prevErrors => ({ ...prevErrors, password: 'Password must contain at least 8 characters including at least one uppercase letter, one number, and one special character' }));
-            return;
-        }
+        //if (!isStrongPassword(password)) {
+        //    setErrors(prevErrors => ({...prevErrors, password: 'Password must contain at least 8 characters including at least one uppercase letter, one number, and one special character' }));
+        //    return;
+        //}
 
-        
-        // Create user data object
-        const userData = {
-            email,
-            password,
-            name,
-            contact,
-            role: selectedRole // Add selectedRole to the user data
-        };
-
-        try {
-            const response = await fetch('/api/user/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userData)
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
-
-            // Handle successful signup
-            console.log('User signed up successfully', response);
-
-            // Clear form fields after successful submission
-            setEmail('');
-            setPassword('');
-            setName('');
-            setContact('');
-            setSelectedRole('');
-        } catch (error) {
-            console.error('Sign up error:', error.message);
-            // Handle error, e.g., show error message to the user
-        }
-
+        await signup(email, password, name, contact);
     }
 
-    // Set the background color to match the Login component
-    document.body.style.backgroundColor = "#ffffff";
-
     return (
-        <div className="flex justify-center py-32 font-jakarta">
-            <form className="signup ml-64 flex flex-col bg-light-blue rounded-md w-128 right-0" onSubmit={handleSubmit}>
-                <h3 className='text-center pt-8 font-semibold text-xl'>Sign Up</h3>
-                <p className='text-center text-sm mb-5'>Create your account.</p>
+        <div className="ml-64">
+        <Container maxWidth="xs">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    minHeight: '100vh',
+                }}
+            >
+                <Typography variant="h4" component="h1" gutterBottom>
+                Staff Registration
+                </Typography>
+                <Typography variant="body1" component="p" gutterBottom>
+                Register new staff members
+                </Typography>
 
-                <label className='indent-6 mt-2 font-medium'>Name</label>
-                <input className='mx-4 h-8 my-1 rounded-md indent-2 bg-text-blue'
-                    type="text"
-                    onChange={(e) => { setName(e.target.value); setErrors(prevErrors => ({ ...prevErrors, name: '' })); }}
-                    value={name}
-                />
-                {errors.name && <div className="mx-4 text-deleteH">{errors.name}</div>}
+                <form onSubmit={handleSubmit} style={{ width: '100%', marginTop: '1rem' }}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        label="Name"
+                        variant="outlined"
+                        value={name}
+                        onChange={(e) => { setName(e.target.value); setErrors(prevErrors => ({ ...prevErrors, name: '' })); }}
+                        error={!!errors.name}
+                        helperText={errors.name}
+                    />
 
-                <label className='indent-6 mt-4 font-medium'>Contact</label>
-                <input className='mx-4 h-8 my-1 rounded-md indent-2 bg-text-blue'
-                    type="text"
-                    onChange={(e) => { setContact(e.target.value); setErrors(prevErrors => ({ ...prevErrors, contact: '' })); }}
-                    value={contact}
-                />
-                {errors.contact && <div className="mx-4 text-deleteH">{errors.contact}</div>}
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        label="Contact"
+                        variant="outlined"
+                        value={contact}
+                        onChange={(e) => { setContact(e.target.value); setErrors(prevErrors => ({ ...prevErrors, contact: '' })); }}
+                        error={!!errors.contact}
+                        helperText={errors.contact}
+                    />
 
-                <label className='indent-6 mt-4 font-medium'>Email</label>
-                <input className='mx-4 h-8 my-1 rounded-md indent-2 bg-text-blue'
-                    type="email"
-                    onChange={(e) => { setEmail(e.target.value); setErrors(prevErrors => ({ ...prevErrors, email: '' })); }}
-                    value={email}
-                />
-                {errors.email && <div className="mx-4 text-deleteH">{errors.email}</div>}
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        label="Email"
+                        variant="outlined"
+                        type="email"
+                        value={email}
+                        onChange={(e) => { setEmail(e.target.value); setErrors(prevErrors => ({ ...prevErrors, email: '' })); }}
+                        error={!!errors.email}
+                        helperText={errors.email}
+                    />
 
-                <label className='indent-6 mt-4 font-medium'>Password</label>
-                <input className='mx-4 h-8 my-1 rounded-md indent-2 bg-text-blue'
-                    type="password"
-                    onChange={(e) => { setPassword(e.target.value); setErrors(prevErrors => ({ ...prevErrors, password: '' })); }}
-                    value={password}
-                />
-                {errors.password && <div className="mx-4 text-deleteH">{errors.password}</div>}
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        label="Password"
+                        variant="outlined"
+                        type="password"
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value); setErrors(prevErrors => ({ ...prevErrors, password: '' })); }}
+                        error={!!errors.password}
+                        helperText={errors.password}
+                    />
 
-                <label className='indent-6 mt-4 font-medium'>User Role</label>
-                <div className="ml-4">
-                    <label className="block">
-                        <input
-                            type="radio"
-                            value="inventory handler"
-                            checked={selectedRole === "inventory handler"}
+                    <FormControl component="fieldset" style={{ marginTop: '1rem' }}>
+                        <FormLabel component="legend">User Role</FormLabel>
+                        <RadioGroup
+                            value={selectedRole}
                             onChange={(e) => setSelectedRole(e.target.value)}
-                        />
-                        <span className='ml-2'>Inventory Handler</span>
-                    </label>
-                    <label className="block">
-                        <input
-                            type="radio"
-                            value="pharmacist"
-                            checked={selectedRole === "pharmacist"}
-                            onChange={(e) => setSelectedRole(e.target.value)}
-                        />
-                        <span className='ml-2'>Pharmacist</span>
-                    </label>
-                    <label className="block">
-                        <input
-                            type="radio"
-                            value="prescription handler"
-                            checked={selectedRole === "prescription handler"}
-                            onChange={(e) => setSelectedRole(e.target.value)}
-                        />
-                        <span className='ml-2'>Prescription Handler</span>
-                    </label>
-                    <label className="block">
-                        <input
-                            type="radio"
-                            value="cashier"
-                            checked={selectedRole === "cashier"}
-                            onChange={(e) => setSelectedRole(e.target.value)}
-                        />
-                        <span className='ml-2'>Cashier</span>
-                    </label>
-                    <label className="block">
-                        <input
-                            type="radio"
-                            value="manager"
-                            checked={selectedRole === "manager"}
-                            onChange={(e) => setSelectedRole(e.target.value)}
-                        />
-                        <span className='ml-2'>Manager</span>
-                    </label>
-                </div>
+                        >
+                            <FormControlLabel value="inventory handler" control={<Radio />} label="Inventory Handler" />
+                            <FormControlLabel value="pharmacist" control={<Radio />} label="Pharmacist" />
+                            <FormControlLabel value="prescription handler" control={<Radio />} label="Prescription Handler" />
+                            <FormControlLabel value="cashier" control={<Radio />} label="Cashier" />
+                            <FormControlLabel value="manager" control={<Radio />} label="Manager" />
+                        </RadioGroup>
+                    </FormControl>
 
-                <button className='bg-login3 mx-auto px-16 py-2 rounded-md my-10 font-black cursor-pointer hover:bg-login2 hover:text-white hover:shadow-lg'>SIGN UP</button>
-            </form>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        disabled={isLoading}
+                        fullWidth
+                        style={{ marginTop: '1rem' }}
+                    >
+                        {isLoading ? "Signing up..." : "REGISTER"}
+                    </Button>
+
+                    {error && <Typography variant="body2" color="error" gutterBottom style={{ marginTop: '1rem' }}>
+                        {error}
+                    </Typography>}
+                </form>
+            </Box>
+        </Container>
         </div>
     )
 }
